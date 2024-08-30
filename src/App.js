16e5e87017d68800b4921f893f4d0f6fc603f6a8
Mohammad Hesham Farhan <moh.hesham.f@gmail.com>
2024-08-30 15:27:20 +0300

    initialize the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header>
+        <h1>Task Management</h1>
+      </header>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
